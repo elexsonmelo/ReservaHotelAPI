69c0f774b448ddb0c2d0aff22f35e5361487c0f6
xmelo_meli <elexson.melo@mercadolivre.com>
2024-02-15 09:48:36 -0300

    Remocao do metodo validarAntecedencia da classe DataUtilService

--- src/main/java/com/example/reservahotelapi/Service/DataUtilService.java
+++ src/main/java/com/example/reservahotelapi/Service/DataUtilService.java
@@ -13,19 +13,8 @@ public class DataUtilService {
     public class ValidadorReserva {
 
         private void validarReserva(Reserva reserva) throws Exception {
-            validarAntecedencia(reserva);
             validarDuracao(reserva);
             validarDisponibilidadeQuarto(reserva);
-        }
-
-        private void validarAntecedencia(Reserva reserva) throws Exception {
-            LocalDate hoje = LocalDate.now();
-            if (reserva.getDataEntrada().isBefore(hoje.plusDays(1))) {
-                throw new Exception("A reserva deve ser feita com no mínimo 1 dia de antecedência");
-            }
-            if (reserva.getDataEntrada().isAfter(hoje.plusDays(30))) {
-                throw new Exception("A reserva não pode ser solicitada com mais de 30 dias de antecedência");
-            }
         }
 
         private void validarDuracao(Reserva reserva) throws Exception {
@@ -33,7 +22,6 @@ public class DataUtilService {
                 throw new Exception("A estadia não pode ser superior a 3 dias");
             }
         }
-
         private void validarDisponibilidadeQuarto(Reserva reserva) throws Exception {
             if (!reserva.getQuarto().getEstaDisponivel()) {
                 throw new Exception("O quarto já está reservado para as datas solicitadas");
